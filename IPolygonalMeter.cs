80fac9aa67951ffe1cbf820d44ba1a6e1cf3ea89
BerdichevskiyM <74819363+BerdichevskiyM@users.noreply.github.com>
2022-05-27 13:03:35 +0400

    smallFixes.v1

--- IPolygonalMeter.cs
+++ IPolygonalMeter.cs
@@ -11,7 +11,7 @@ namespace SquareMeter
         float this[int sideI] { get; set; }
 
         /// <summary>
-        /// If side index is wrong, throw exception.
+        /// If side index is wrong, throws exception.
         /// </summary>
         /// <param name="sideI"></param>
         void CheckSideI(int sideI);
